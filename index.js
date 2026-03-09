1c91436c69ad7b3ce631bf026bf16e1ebf5f1011
kaewkamol-a4 <kankaewkamol.a4@gmail.com>
2026-03-09 14:16:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(express.json());
 // 🔑 ตั้งค่าตรงนี้ก่อนรัน
 // ===================================================
 const CONFIG = {
-  CHANNEL_ACCESS_TOKEN: 'YOUR_CHANNEL_ACCESS_TOKEN', // จาก Line Developers Console
-  CHANNEL_SECRET: 'YOUR_CHANNEL_SECRET',             // จาก Line Developers Console
-  SHOP_LINK: 'https://shop.line.me/@160veikw',       // ลิงก์ MyShop ของคุณ
-  WALLPAPER_DRIVE_LINK: 'YOUR_GOOGLE_DRIVE_LINK',    // ลิงก์ Google Drive โฟลเดอร์วอลเปเปอร์
+  CHANNEL_ACCESS_TOKEN: 'IHEf4nxyWd4DyykrHPrjXyukregJgMvAZk50PIl4OVKReHB4gSYG+kFLulQ96Dr7rl6KA4S8Xc1jjOnuRQEzjXxmYpB3rx2xGNTNaLcBcyWK7ADP2HJXioZBWnwqmC1ajL/m5vx1VW0L8fp8POi5/gdB04t89/1O/w1cDnyilFU=', // จาก Line Developers Console
+  CHANNEL_SECRET: '1daf43d02a7f95950368d57f8f368be4',             // จาก Line Developers Console
+  SHOP_LINK: 'https://shop.line.me/@969bhktx',       // ลิงก์ MyShop ของคุณ
+  WALLPAPER_DRIVE_LINK: '',    // ลิงก์ Google Drive โฟลเดอร์วอลเปเปอร์
 };
 
 // ===================================================
